5c473de60edf7c1ac7aafccd6538ef9b8ea09378
Simon2207-u <sumerez@escuelasproa.edu.ar>
2024-09-03 08:59:12 -0300

    Add files via upload

---
+++ Primer programa en python.py
@@ -0,0 +1,9 @@
+#desarrollar un algoritmo que permita ingresar 2 importes, luego sumar ambos y el resultado
+
+#explicacion:
+#VARIABLE convierte el tipo de datos de string (cadena) a float (real) el valor 
+
+importe1=float(input("ingrse primer importe: "))
+importe2=float(input("ingrse segundo importe: "))
+total=importe1+importe2
+print("el total es: "), total
