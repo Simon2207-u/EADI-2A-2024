8fb2bb0c3384b280be29b95490c7abee7cd84bae
Simon2207-u <sumerez@escuelasproa.edu.ar>
2024-09-03 09:44:14 -0300

    Add files via upload

---
+++ 03-09-24.py
@@ -0,0 +1,16 @@
+DNI=49963456 #Int
+Edad=14 #Int
+Dirección="San Martín 1750" #String
+Localidad="San Marcos Sierras" #String
+CódigoPostal=5282 #Int
+Altura=1.51 #float
+print ("El valor de la variable es:", DNI)
+print ("El valor de la variable es:", Edad)
+print ("El valor de la variable es:", Dirección)
+print ("El valor de la variable es:", Localidad)
+print ("El valor de la variable es:", CódigoPostal)
+print ("El valor de la variable es:", Altura)
+
+
+
+
