4c49bc737250c8db3c176fad40aa7cd38ade9de0
Simon2207-u <sumerez@escuelasproa.edu.ar>
2024-09-10 15:20:19 -0300

    Add files via upload

---
+++ 10-09-a2.py
@@ -0,0 +1,6 @@
+altura1=float(input ("ingrse primera altura"))
+altura2=float(input("ingrse segunda altura"))
+altura3=float(input("ingrse tercera altura"))
+altura4=float(input("ingrse cuarta altura"))
+p=(altura1+altura2+altura3+altura4)/4
+print("altura promedio es:", p)
---
+++ 10-09.py
@@ -0,0 +1,4 @@
+anioNacimineto=int(input("ingrse su año de nacimineto"))
+anioActual=int(input("ingrese su año de nacimiento"))
+edad=anioActual-anioNacimineto
+print("edad es:", edad)
