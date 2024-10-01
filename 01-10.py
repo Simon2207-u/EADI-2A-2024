1b5259bb9523770d9637c64c5fe8b0590e443848
Simon2207-u <sumerez@escuelasproa.edu.ar>
2024-10-01 14:51:23 -0300

    Add files via upload

---
+++ 01-10.py
@@ -0,0 +1,12 @@
+#int
+Edad=14
+#float
+Promedio=7.2
+#string
+Dirección="San Martín 1750"
+#boolean
+Estudiante=True
+print("edad es:", Edad)
+print("promedio es:", Promedio)
+print("Dirección es:", Dirección)
+print("el valor que contiene la variable Estudiante es:", True)
