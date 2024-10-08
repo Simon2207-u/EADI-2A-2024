975080a346b6a3260fda11ddbbb0c76b395716b7
Simon2207-u <sumerez@escuelasproa.edu.ar>
2024-10-08 14:40:51 -0300

    Add files via upload

---
+++ 08-10.py
@@ -0,0 +1,8 @@
+nombre="Simón"
+dirección="San Martín 1750"
+estudiante=True
+edad=14
+altura=1.51
+listaIntereses=["futbol", "Jugar con el celu"]
+listaMisDatos=[nombre, dirección, estudiante, edad, altura, listaIntereses]
+print("mis datos personales son:", listaMisDatos)
